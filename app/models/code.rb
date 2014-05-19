e06ed0c9bf33c93549b245ca8fa90d2c2b9b33af
belhyun <belhyun@gmail.com>
2014-05-20 02:43:15 +0900

    update

--- app/models/code.rb
+++ app/models/code.rb
@@ -3,6 +3,11 @@ class Code
     :not_enough_money => "보유 금액이 부족합니다.",
     :success => "success",
     :buy_transaction_fail => "buy transaction fail",
-    :stock_money_zero => "stock money zero"
+    :stock_money_zero => "stock money zero",
+    :stock_amounts_is_invaild => "Stock을 정확히 입력해 주세요",
+    :user_has_no_stock => "보유 stock이 없습니다.",
+    :user_stock_lack => "보유 stock이 부족합니다."
   }
+
+  COMMISION = 0.2
 end
