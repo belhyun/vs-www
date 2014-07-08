332bb9824ea0b9cbad6dc493690c4e485de2ce0d
belhyun <belhyun@gmail.com>
2014-07-08 13:26:39 +0900

    update

--- app/admin/user_stock.rb
+++ app/admin/user_stock.rb
@@ -1,2 +1,11 @@
 ActiveAdmin.register UserStock do
+  form do |f|
+    f.inputs "UserStocks" do
+      f.input :stock, :as => :select, :collection => Stock.all.each{|stock| stock.name = "#{stock.issue.title} - #{stock.name}"}
+      f.input :issue
+      f.input :user, :as => :select, :collection => User.all.each{|user| user.name = "#{user.email}(#{user.nickname})"}
+      f.input :stock_amounts
+    end
+    f.buttons
+  end
 end
