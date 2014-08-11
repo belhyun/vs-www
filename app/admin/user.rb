940894667339647ddd86409e63264fc1fc0143e8
belhyun <belhyun@gmail.com>
2014-08-11 16:13:31 +0900

    update

--- app/admin/user.rb
+++ app/admin/user.rb
@@ -1,3 +1,12 @@
 ActiveAdmin.register User do
-
+  form do |f|
+    f.inputs "User" do
+      f.input :email
+      f.input :money
+      f.input :nickname
+      f.input :is_push, :as => :boolean, :label => "푸쉬여부"
+      f.input :is_admin, :as => :boolean, :label => "어드민여부"
+    end
+    f.buttons
+  end
 end
